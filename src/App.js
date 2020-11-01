382c1ceb8df16e2daafb19a68428fef990eaf01c
noel2017mdala <noelmdala2017@gmail.com>
2020-11-01 17:41:21 +0200

    adding comments to the app.js file

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import {useSelector, useDispatch} from 'react-redux';
 import {createFetch} from './Store/Actions/ActionCreator';
 
 const App = (props) => {
+
+  //dispatch is used to call an action
   const dispatch = useDispatch();
 
+  /*
+  call the dispatches the async function
+   immediately after the page has finished loading
+  */
   useEffect(() =>{
     const loadApi = async () =>{
       await dispatch(createFetch());
@@ -13,6 +19,7 @@ const App = (props) => {
     setTimeout(loadApi, 2000);
   }, [dispatch]);
 
+// Holds the current state of the site
   const select = useSelector((data) =>{
     return data;
   });
@@ -31,7 +38,13 @@ const App = (props) => {
             <button>Search</button>
           </div>
         </div>
+        
         {
+          /*
+          check wether the fetch api has finished prosessing the request if 
+          the page has  it will display the data else the page will 
+          continue loading
+          */
           !select.fetch.data ? <p>Loading ...</p> : select.fetch.data.map((data, i) => (
         <div key={data.id}>
           <img  src={data.urls.full} alt={data.alt_description} height='400' width='400' />
